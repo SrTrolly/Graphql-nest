bf40473bd19bcec5884eb5dd0cc14d74a142d753
Nicolas Inostroza <ninostrozacordova2016@gmail.com>
2022-10-15 13:57:25 -0300

    Iniciando seccion de Auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { ApolloDriverConfig } from '@nestjs/apollo';
 import { ApolloDriver } from '@nestjs/apollo/dist/drivers';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { join } from 'path';
 import { ItemsModule } from './items/items.module';
 
 @Module({
   imports: [
+
+    ConfigModule.forRoot(),
+
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: false,
@@ -16,6 +21,19 @@ import { ItemsModule } from './items/items.module';
         ApolloServerPluginLandingPageLocalDefault
       ]
     }),
+
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      autoLoadEntities: true
+    }),
+
+
     ItemsModule
   ],
   controllers: [],
